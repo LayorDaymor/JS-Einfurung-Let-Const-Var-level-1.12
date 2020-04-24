8c7e7cd5328aad599924d9e096b33bc14d050d82
SuperCode <supercode@SuperCodes-MacBook-Air.local>
2020-04-25 00:08:08 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+var score= 5+5 * 10;
+console.log(score)
+
+var score = (5 + 5) * 10;
+console.log(score);
+
+var score = 0;
+score = score + 10;
+console.log(score);
+
+score += 10;
+console.log(score);
+
+/* nkrementierung und Dekrementierungen */
+/* erste Methode */
+var increment= 19
+increment =increment +1
+console.log(increment)
+
+/* Zweite Methode */
+var increment= 19
+increment+=1
+console.log(increment);
+
+/* Dritte Methode */
+var increment= 19
+increment++
+console.log(increment);
+
+/* Vierte Methode */
+var increment= 21
+increment--
+console.log(increment);
